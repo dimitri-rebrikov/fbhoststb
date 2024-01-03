cb8cf9f4ff520751db7e7342aa58138e3e2cba92
miniair <miniair@miniair.fritz.box>
2024-01-03 20:17:23 +0100

    updated libraries, bugfix
    1. adjusted code to the current version of fritzconnect and telegram bot
    2. fixed crash if the mac address is empty (it is for the VPN connections)

--- fbhoststb.py
+++ fbhoststb.py
@@ -1,9 +1,10 @@
-import fritzconnection as fc
+from fritzconnection.lib.fritzhosts import FritzHosts
 import json
 import telegram
 import os
 from time import localtime, strftime, sleep
 import sys
+import asyncio
 
 # function to provide formatted time
 def getFormattedTime():
@@ -23,94 +24,102 @@ def detectNetworkName(networkMapping, ip):
 def getHistoryFileName():
     return '{}.dat'.format(strftime("%Y-%m-%d", localtime()))
 
-# default path to the config file
-configFilePath='./fbhoststb.cfg'
-# the path to the config file might be provided as 1st parameter
-if len(sys.argv) > 1:
-    configFilePath=sys.argv[1]
-# load config
-with open(configFilePath, 'r') as configFile:
-    config = json.load(configFile)
+async def main():
+    # default path to the config file
+    configFilePath='./fbhoststb.cfg'
+    # the path to the config file might be provided as 1st parameter
+    if len(sys.argv) > 1:
+        configFilePath=sys.argv[1]
+    # load config
+    with open(configFilePath, 'r') as configFile:
+        config = json.load(configFile)
 
-# create history dir
-os.makedirs(config['historyDir'], exist_ok=True)
+    # create history dir
+    os.makedirs(config['historyDir'], exist_ok=True)
 
-storage={}
-# load stored hosts
-if os.path.isfile(config['storage']):
-    with open(config['storage'], 'r') as f:
-        storage=json.load(f)
+    storage={}
+    # load stored hosts
+    if os.path.isfile(config['storage']):
+        with open(config['storage'], 'r') as f:
+            storage=json.load(f)
 
-# initalize Fritz Box connection
-fh = fc.FritzHosts(**config['fritzbox'])
-# initialize Telegram connection
-bot = telegram.Bot(token = config['telegram']['token'])
+    # initalize Fritz Box connection
+    fh = FritzHosts(**config['fritzbox'])
+    # initialize Telegram connection
+    bot = telegram.Bot(token = config['telegram']['token'])
 
-while True: # infinite loop
-    # load current hosts from FritzBox
-    try:
-        hosts = fh.get_hosts_info()
-    except :
-        # the errors happen sporadically 
-        print('{}: got Exception from FritzBox get_hosts_info(): {}'
-            .format(getFormattedTime(), sys.exc_info()[0]))
-        # prevent high frequent polling if the error happens permanently
-        sleep(10)
-        # retry
-        continue
-    # compare stored with current and update
-    changed=False
-    for index, host in enumerate(hosts):
-        mac = host.get('mac')
-        ip = host.get('ip')
-        name = host.get('name')
-        # decode the connection status
-        status = 'connected' if host.get('status') == '1' else 'disconnected'
-        # get the stored host info 
-        storedHost = storage.get(mac)
-        if storedHost == None or storedHost.get('ip') != ip or storedHost.get('status') != status:
-            # if there is no stored host or the ip or status has changed 
-            #
-            # get the interface (i.e. LAN/WiFI etc) from details           
-            hostDetails = fh.get_specific_host_entry(host.get('mac'))
-            interface = hostDetails.get('NewInterfaceType')
-            # create the new host entry
-            newHost = {
-                'ip': ip, 
-                'name': name,
-                'network': detectNetworkName(config['networkMapping'],ip),
-                'status': status, 
-                'time': getFormattedTime(),
-                'interface': config['interfaceMapping'].get(interface, interface)
-            }
-            # store the new host entry (overwriting the old one if there)
-            storage[mac] = newHost
-            # set the flag that the data has changed
-            changed = True
-            # prepare the message send it to stdout
-            msg = '{name} {status} @ {network}({interface})'.format(**newHost)
-            print('{}: {}'.format(getFormattedTime(), msg))
-            # sent the message over Telegram Bot
-            bot.send_message(
-                chat_id = config['telegram']['chatId'],
-                text = msg
-            )
-            # write into history
-            with open('{dir}/{file}'.format(
-                    dir=config['historyDir'], 
-                    file=getHistoryFileName()),'a') as f:
-                historyEntry = {
-                    'mac':mac,  
-                    'message':msg
+    while True: # infinite loop
+        # load current hosts from FritzBox
+        try:
+            hosts = fh.get_hosts_info()
+        except :
+            # the errors happen sporadically 
+            print('{}: got Exception from FritzBox get_hosts_info(): {}'
+                .format(getFormattedTime(), sys.exc_info()[0]))
+            # prevent high frequent polling if the error happens permanently
+            sleep(10)
+            # retry
+            continue
+        # compare stored with current and update
+        changed=False
+        for index, host in enumerate(hosts):
+            # print(host)
+            mac = host.get('mac')
+            if not mac:
+                # ignore devices without mac 
+                continue
+            ip = host.get('ip')
+            name = host.get('name')
+            # decode the connection status
+            status = 'connected' if host.get('status') == True else 'disconnected'
+            # get the stored host info 
+            storedHost = storage.get(mac)
+            if storedHost == None or storedHost.get('ip') != ip or storedHost.get('status') != status:
+                # if there is no stored host or the ip or status has changed 
+                #
+                # get the interface (i.e. LAN/WiFI etc) from details           
+                hostDetails = fh.get_specific_host_entry(host.get('mac'))
+                interface = hostDetails.get('NewInterfaceType')
+                # create the new host entry
+                newHost = {
+                    'ip': ip, 
+                    'name': name,
+                    'network': detectNetworkName(config['networkMapping'],ip),
+                    'status': status, 
+                    'time': getFormattedTime(),
+                    'interface': config['interfaceMapping'].get(interface, interface)
                 }
-                historyEntry.update(newHost)
-                json.dump(historyEntry, f)
-                f.write('\n')
+                # store the new host entry (overwriting the old one if there)
+                storage[mac] = newHost
+                # set the flag that the data has changed
+                changed = True
+                # prepare the message send it to stdout
+                msg = '{name} {status} @ {network}({interface})'.format(**newHost)
+                print('{}: {}'.format(getFormattedTime(), msg))
+                # sent the message over Telegram Bot
+                async with bot:
+                    await bot.send_message(
+                        chat_id = config['telegram']['chatId'],
+                        text = msg
+                    )
+                # write into history
+                with open('{dir}/{file}'.format(
+                        dir=config['historyDir'], 
+                        file=getHistoryFileName()),'a') as f:
+                    historyEntry = {
+                        'mac':mac,  
+                        'message':msg
+                    }
+                    historyEntry.update(newHost)
+                    json.dump(historyEntry, f)
+                    f.write('\n')
 
-    if changed:
-        # if there are changes
-        # store the data 
-        with open(config['storage'], 'w') as f:
-            json.dump(storage, f, sort_keys=True, indent=4)
- 
-    sleep(30)
+        if changed:
+            # if there are changes
+            # store the data 
+            with open(config['storage'], 'w') as f:
+                json.dump(storage, f, sort_keys=True, indent=4)
+     
+        sleep(30)
+
+asyncio.run(main())
